329a01359da6af55a99ff27ba8fcc73e0f2ac615
MartinStefanec <martin.stefanec@azet.sk>
2017-04-21 18:28:23 +0200

    3. filled structures

---
+++ naplneniestruktur.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <time.h>
+#include<math.h>
+
+typedef struct{
+	float x;
+	float y;
+}POINT;
+
+typedef struct{
+	POINT c;
+	float r;
+}CIRC;
+
+main(){
+	CIRC c1,c2;
+	//info o prvej kruznici
+	printf("Zadajte suradnice stredu prvej kruznice: \n");
+	scanf("%f%f",&c1.c.x,&c1.c.y);
+	printf("Zadajte polomer prvej kruznice (kladne cislo): \n"); 
+	scanf("%f",&c1.r);
+	
+	//info o druhej kruznici
+	printf("Zadajte suradnice stredu druhej kruznice: \n");
+	scanf("%f%f",&c2.c.x,&c2.c.y);
+	printf("Zadajte polomer druhej kruznice (kladne cislo): \n"); 
+	scanf("%f",&c2.r);
+	
+}
